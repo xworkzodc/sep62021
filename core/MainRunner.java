246640784c880a1d6af1a59b02fddaa9f6bbd3ec
Om <om@Apples-MacBook-Pro.local>
2021-10-01 09:12:04 +0530

    This is my first commit

---
+++ core/MainRunner.java
@@ -0,0 +1,12 @@
+class MainRunner{
+
+public static void main(String[] values){
+  System.out.println(values.length);
+     for(int index=0;index<values.length;index++)
+     {
+       System.out.println(values[index]);
+     }
+
+}
+
+}
